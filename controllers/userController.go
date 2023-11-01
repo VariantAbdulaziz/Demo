c0e8bdfa10bd4d9c384498e57944d3db374129ee
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2023-11-01 07:31:02 +0300

    add login controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/variant-abdulaziz/initializers"
 	"github.com/variant-abdulaziz/models"
 	"golang.org/x/crypto/bcrypt"
@@ -48,4 +50,59 @@ func SignUp(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, gin.H{})
+}
+
+func Login(c *gin.Context) {
+	// get data from body
+	var body struct {
+		Username string
+		Password string
+	}
+
+	if c.BindJSON(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "failed to read body",
+		})
+		return
+	}
+
+	// get user specified from db
+	var user = models.User{Username: body.Username}
+	initializers.DB.First(&user)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "0: incorrect username/password",
+		})
+		return
+	}
+
+	// compare the stored hash with the provided password
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "1: incorrect username/password",
+		})
+		return
+	}
+
+	// generate a jwt token
+	key := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256, jwt.MapClaims{
+			"sub": user.ID,
+		})
+	tokenString, err := token.SignedString([]byte(key))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "server error",
+		})
+		return
+	}
+
+	// respond
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("token", tokenString, 60*60*24*30, "", "", false, true)
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/variant-abdulaziz/controllers"
 	"github.com/variant-abdulaziz/initializers"
 )
 
@@ -13,10 +14,7 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/api/signup", controllers.SignUp)
+	r.POST("/api/login", controllers.Login)
 	r.Run()
 }
